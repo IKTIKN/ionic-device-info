895256077ff788558866262d532bc790ea30aff3
IKTIKN <developermarc@outlook.com>
2023-04-10 02:38:06 +0200

    Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-device-info',
+  appId: 'iktikn.ionic.device',
+  appName: 'Device info',
   webDir: 'www',
   bundledWebRuntime: false
 };
